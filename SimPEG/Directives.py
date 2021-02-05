d50b7c0cce364c8174bc299a8b7775db212c7f27
Xiaolong Wei <56940558+xiaolongw1223@users.noreply.github.com>
2021-02-04 21:33:45 -0600

    Update Directives.py

--- SimPEG/Directives.py
+++ SimPEG/Directives.py
@@ -2022,10 +2022,14 @@ class Update_IRLS_Joint(InversionDirective):
                 print("delta phim: {0:6.3e}, {0:6.3e}".format(self.f_joint_change[0], self.f_joint_change[-1]))
 
 
-            print("f_joint_change:", self.f_joint_change)
+            print("f_joint_change: ", self.f_joint_change)
+            stop_alpha = norm(self.opt.xc - self.opt.x_last) / norm(self.opt.x_last)
+            print("stopping tolerance: ", stop_alpha)
+            
             if np.all([
-                self.f_joint_change[0] < self.f_min_change,
-                self.f_joint_change[-1] < self.f_min_change,
+#                 self.f_joint_change[0] < self.f_min_change,
+#                 self.f_joint_change[-1] < self.f_min_change,
+                stop_alpha < self.f_min_change,
                 np.abs(1. - self.invProb.phi_d_joint[0] / self.target[0]) < self.beta_tol,
                 np.abs(1. - self.invProb.phi_d_joint[-1] / self.target[-1]) < self.beta_tol,
                 self.IRLSiter > 1
@@ -2037,7 +2041,6 @@ class Update_IRLS_Joint(InversionDirective):
                     )
                 self.opt.stopNextIteration = True
                 return
-
 
             self.f_joint = phim_new_joint
             self.updateBeta = True
